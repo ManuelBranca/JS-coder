eefff29f40b5a519e666da5c82abf059dcc4b933
Manuel Branca <manubranca0227@gmail.com>
2023-07-26 20:39:42 -0300

    Botones funcionales y sweet alerts

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const productos=[
+const productos = [
     {
         cantidad: 0,
         id: 1,
-        nombre:"574 - New Balance",
+        nombre: "574 - New Balance",
         precio: 33000,
         urlImg: "../img/new-balance.jpg"
     },
     {
         cantidad: 0,
-        id:2,
+        id: 2,
         nombre: "Air Max - Nike",
         precio: 40000,
         urlImg: "../img/airmax-nike.jpg"
     },
     {
         cantidad: 0,
-        id:3,
+        id: 3,
         nombre: "Galaxy 6 - Adidas",
         precio: 29000,
         urlImg: "../img/galaxy6-adidas.jpg"
@@ -24,70 +24,75 @@ const productos=[
 const productos2 = [
     {
         cantidad: 0,
-        id:4,
+        id: 4,
         nombre: "Xray 2 - Puma",
         precio: 25000,
         urlImg: "../img/puma-xray2.jpg"
     },
     {
         cantidad: 0,
-        id:5,
+        id: 5,
         nombre: "Superstar Clasicas - Adidas",
         precio: 27000,
         urlImg: "../img/superstar-clasicas.jpg"
     },
     {
         cantidad: 0,
-        id:6,
-        nombre: "Urbana de cuero - Dino Butelli",
+        id: 6,
+        nombre: "Urbana de Cuero - Dino Butelli",
         precio: 32000,
         urlImg: "../img/urbanadecuero-dinobutelli.jpg"
     }
 ]
-const ArrayDeProductos = [...productos,...productos2];
+const ArrayDeProductos = [...productos, ...productos2];
 console.log(ArrayDeProductos);
 
-localStorage.setItem("productos",JSON.stringify(productos))
+localStorage.setItem("productos", JSON.stringify(productos))
 console.log(productos)
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const containerProductos = document.getElementById("containerProductos");
-function mostrarProductos(array){
-    containerProductos.innerHTML="";
+
+function mostrarProductos(array) {
+    containerProductos.innerHTML = "";
     array.forEach(e => {
-        const article = document.createElement("article")
-        article.className= "container col-3 card mod m-3"
-        article.innerHTML=`
-        <div class="mt-2 mb-2">
-            <img class="img-fluid" src="${e.urlImg}" alt="Foto del producto principal">
-            <h3>${e.nombre}</h3>
-            <p>$ ${e.precio}</p>
-            <input type="button" value="Agregar al Carrito" class="btn btn-personalizado" id="${e.id}">
-        </div>
-        `
+        const article = document.createElement("article");
+        article.className = "container col-3 card mod m-3";
+        article.innerHTML = `
+            <div class="mt-2 mb-2">
+                <img class="img-fluid" src="${e.urlImg}" alt="Foto del producto principal">
+                <h3>${e.nombre}</h3>
+                <p>$${e.precio}</p>
+                <input type="button" value="Agregar al Carrito" class="btn btn-personalizado" id="${e.id}">
+            </div>
+        `;
         containerProductos.appendChild(article);
-        document.getElementById(e.id).addEventListener("click",(event) =>{
-            if(carrito.some(el=>el.id==e.id)){
-            carrito.find(p=>p.id==e.id).cantidad++;
-            }else{
-                carrito.push({...e,cantidad:1});                
+
+        document.getElementById(e.id).addEventListener("click", (event) => {
+            if (carrito.some(el => el.id == e.id)) {
+                carrito.find(p => p.id == e.id).cantidad++;
+            } else {
+                carrito.push({ ...e, cantidad: 1 });
             }
-            localStorage.setItem("carrito",JSON.stringify(carrito));
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+
+            Swal.fire({
+                position: 'top-end', 
+                icon: 'success',
+                title: 'Producto agregado al carrito correctamente',
+                showConfirmButton: false,
+                timer: 700 
+            });
         });
     });
 }
-mostrarProductos(ArrayDeProductos)
 
-localStorage.setItem("carrito2",JSON.stringify(productos2))
+mostrarProductos(ArrayDeProductos);
+
+localStorage.setItem("carrito2", JSON.stringify(productos2))
 console.log(productos2)
 
 
-localStorage.setItem("arrayCarrito",JSON.stringify( ArrayDeProductos))
+localStorage.setItem("arrayCarrito", JSON.stringify(ArrayDeProductos))
 
-// const span = document.getElementById("span");
-// span.addEventListener("keypress",cambiarNumero)
-
-// function cambiarNumero {
-
-// }
--- js/micarrito.js
+++ js/micarrito.js
@@ -10,7 +10,7 @@ function pintarCarrito(array) {
         <div class="mt-2 mb-2">
             <img class="img-fluid" src="${e.urlImg}" alt="Foto del producto principal">
             <h3>${e.nombre}</h3>
-            <p>$ ${e.precio}</p>
+            <p>$${e.precio * e.cantidad}</p>
             <p>Cantidad: ${e.cantidad}</p>
             <input type="button" value="Eliminar" class="btn btn-personalizado btnEliminar" data-id="${e.id}">
         </div>
@@ -34,8 +34,8 @@ if (carrito.length !== 0) {
     const buttonContainer = document.createElement("div");
     buttonContainer.className = "container gap-5 d-flex justify-content-center";
     buttonContainer.innerHTML = `
-    <button id="btn-Comprar"class="btn-compra btn">Comprar</button>
-    <button class="btn-eliminar btn">Eliminar Todo</button>
+    <button id="btn-Comprar" class="btn-compra btn mt-1">Comprar</button>
+    <button class="btn-eliminar btn mt-1">Eliminar Todo</button>
     `;
     containerBtn.appendChild(buttonContainer);
 
@@ -57,29 +57,33 @@ function eliminarTodo() {
 }
 
 const btnComprar = document.getElementById("btn-Comprar");
-btnComprar.addEventListener("click",comprarTodo);
+btnComprar.addEventListener("click", comprarTodo);
 
-function calcularValor(array){
-let sumatoria = 0;
-let cantidad = 0;
-let total = 0;
-array.forEach ( e =>{
-cantidad = e.cantidad;
-total = e.precio*cantidad
-sumatoria +=total;
-});
-return sumatoria;
+function calcularValor(array) {
+    let sumatoria = 0;
+    let cantidad = 0;
+    let total = 0;
+    array.forEach(e => {
+        cantidad = e.cantidad;
+        total = e.precio * cantidad
+        sumatoria += total;
+    });
+    return sumatoria;
 }
 
 
-function comprarTodo(){
-    if (carrito !== 0){
-    const totalCompra = calcularValor(carrito)
-    alert("Se han comprado todos los productos ingresados en el carrito por un valor de: $ " + totalCompra)
+function comprarTodo() {
+    if (carrito !== 0) {
+        const totalCompra = calcularValor(carrito)
+        Swal.fire(
+            'Se ha completado la compra correctamente!',
+            'El monto pagado fue de $' + totalCompra,
+            'success'
+        )
     } else {
         alert("El carrito esta vacio")
     }
     carrito = [];
     pintarCarrito(carrito);
-    localStorage.setItem("carrito",JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
